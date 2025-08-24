6778fd4328a4e50136e6c826370e92a82d18c1a9
shuang <shuang@tesobe.com>
2025-08-24 17:00:50 +0800

    feat:Improve performance of function useReduxLiteStore

--- src/hook.ts
+++ src/hook.ts
@@ -1,4 +1,4 @@
-import { useContext, useMemo } from 'react';
+import { useContext, useMemo, useRef } from 'react';
 import { StoreContext } from './context';
 import { StateFromInit, ReduxLiteStore, CapitalizeString, Dispatchers } from './types';
 
@@ -10,31 +10,33 @@ import { StateFromInit, ReduxLiteStore, CapitalizeString, Dispatchers } from './
  */
 export const useReduxLiteStore = <T extends Record<string, any>>(): ReduxLiteStore<StateFromInit<T>> => {
   const context = useContext(StoreContext);
+  const contextRef = useRef(context!);
   if (!context) {
     throw new Error('useReduxLiteStore must be used within a ReduxLiteProvider.');
   }
+  contextRef.current = context;
 
   // Memoize dispatchers to prevent re-creation on every render
   const dispatchers = useMemo(() => {
-    const { dispatch, state: fullState } = context;
-    const dispatcherAcc = Object.keys(fullState).reduce((acc, key) => {
+    const { state} = context;
+    const dispatcherAcc = Object.keys(state).reduce((acc, key) => {
       const capitalizedKey = (key.charAt(0).toUpperCase() + key.slice(1)) as CapitalizeString<typeof key>;
 
       // Create dispatchUser
       acc[`dispatch${capitalizedKey}`] = (payload: any) => {
-        dispatch({ type: key, payload, isPartial: false });
+        contextRef.current.dispatch({ type: key, payload, isPartial: false });
       };
 
       // Create dispatchPartialUser
       acc[`dispatchPartial${capitalizedKey}`] = (payload: any) => {
-        dispatch({ type: key, payload, isPartial: true });
+        contextRef.current.dispatch({ type: key, payload, isPartial: true });
       };
 
       return acc;
     }, {} as { [key: string]: any }); // Use a flexible type for the accumulator
 
     return dispatcherAcc as Dispatchers<StateFromInit<T>>; // Assert the final type
-  }, [context.dispatch, context.state]);
+  }, []);
 
   return { ...(context.state as StateFromInit<T>), ...dispatchers };
 };
